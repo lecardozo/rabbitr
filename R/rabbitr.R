ccc43d1ccd38bf39a34ca0d22414f6eb9457f7eb
Lucas Cardozo <lucasecardozo@gmail.com>
2018-12-14 16:23:33 -0200

    adding more functions in the interface between c and R

--- R/rabbitr.R
+++ R/rabbitr.R
@@ -1,3 +1,48 @@
 #' @useDynLib rabbitr
 #' @importFrom Rcpp sourceCpp
 NULL
+
+Connection <- R6::R6Class(
+    'Connection',
+    public = list(
+        host = NULL,
+        port = NULL,
+        xptr = NULL,
+
+        initialize = function(host='localhost', port=5673) {
+            self$host = host
+            self$port = port
+            self$xptr <- rabbitmq_connect(host, port)
+        }
+    ),
+)
+
+Channel <- R6::R6Class(
+    'Channel',
+    public = list(
+        conn = NULL,
+        basic_consume = function() {
+            while (TRUE) {
+                # call
+            }
+        }
+    )
+)
+
+Queue <- R6::R6Class(
+    'Queue',
+    public = list(
+        declare = function() {
+           # declare queue
+        },
+
+        bind = function(conn) {}
+    )
+)
+
+Exchange <- R6::R6Class(
+    'Exchange',
+    public = list(
+        name = NULL
+    )
+)
--- src/rabbitr.cpp
+++ src/rabbitr.cpp
@@ -5,18 +5,16 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-SEXP connect(std::string host, int port) {
+SEXP rabbitmq_connect(std::string host, int port) {
     int status;
     amqp_socket_t *socket = NULL;
     amqp_connection_state_t conn;
-
-    amqp_bytes_t queue;
 
     conn = amqp_new_connection();
 
     socket = amqp_tcp_socket_new(conn);
 
-    status = amqp_socket_open(socket, "localhost", port);
+    status = amqp_socket_open(socket, host.c_str(), port);
 
     amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
 
@@ -27,4 +25,34 @@ SEXP connect(std::string host, int port) {
     Rcpp::XPtr<amqp_connection_state_t> p(&conn);
 
     return p;
+}
+
+void rabbitmq_declare_queue(SEXP xptr) {
+    amqp_bytes_t queue;
+    amqp_connection_state_t *conn = rabbitmq_get_state(xptr);
+    amqp_queue_declare_ok_t *r = amqp_queue_declare(
+        conn, 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table
+    );
+
+    queue = amqp_bytes_malloc_dup(r->queue);
+    if (queue.bytes == NULL) {
+        Rcout << "Out of memory while copying queue name";
+    }
+}
+
+// [[Rcpp::export]]
+void rabbitmq_consume(SEXP xptr) {
+}
+
+
+amqp_connection_state_t rabbitmq_get_state(SEXP xptr) {
+    void *state = NULL;
+    if (TYPEOF(extPtr) != EXTPTRSXP) {
+      error("Expected an external pointer");
+    }
+    state = (amqp_connection_state_t*) R_ExternalPtrAddr(xptr);
+    if (!state) {
+      error("Rabbitmq is not connected");
+    }
+    return state;
 }
