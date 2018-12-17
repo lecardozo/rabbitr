59c27881235fc8b256b0cfe065ef51792163dd74
Lucas Cardozo <lucasecardozo@gmail.com>
2018-12-17 17:38:24 -0300

    fix segfault on channel_open after XPtr casting

--- src/rabbitr.cpp
+++ src/rabbitr.cpp
@@ -6,19 +6,26 @@
 
 using namespace Rcpp;
 
-amqp_connection_state_t rabbitmq_get_state(SEXP xptr) {
+amqp_connection_state_t_* get_connection_state(SEXP xptr) {
+
     if (TYPEOF(xptr) != EXTPTRSXP) {
         throw std::range_error("Expected an external pointer");
     }
-    Rcpp::XPtr<amqp_connection_state_t> state(xptr);
-    return *state;
+    Rcpp::XPtr<amqp_connection_state_t_> conn(xptr);
+
+    return conn;
+}
+
+void amqp_finalize_connection(amqp_connection_state_t_* conn) {
+    amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
+    amqp_destroy_connection(conn);
 }
 
 // [[Rcpp::export("amqp_connect")]]
 SEXP connect(std::string host, int port) {
     int status;
     amqp_socket_t *socket = NULL;
-    amqp_connection_state_t conn;
+    amqp_connection_state_t_ *conn;
 
     conn = amqp_new_connection();
 
@@ -35,21 +42,20 @@ SEXP connect(std::string host, int port) {
 
     amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
 
-    Rcpp::XPtr<amqp_connection_state_t> p(&conn, true);
-
-    return p;
+    Rcpp::XPtr<amqp_connection_state_t_, PreserveStorage, amqp_finalize_connection> xptr(conn, true);
+    return xptr;
 }
 
 // [[Rcpp::export("amqp_channel_open")]]
 void channel_open(SEXP xptr, int channel) {
-    amqp_connection_state_t conn = rabbitmq_get_state(xptr);
+    amqp_connection_state_t conn = get_connection_state(xptr);
     amqp_channel_open(conn, channel);
 }
 
 // [[Rcpp::export("amqp_queue_declare")]]
 void queue_declare(SEXP xptr) {
     amqp_bytes_t queue;
-    amqp_connection_state_t conn = rabbitmq_get_state(xptr);
+    amqp_connection_state_t conn = get_connection_state(xptr);
     amqp_queue_declare_ok_t *r = amqp_queue_declare(
         conn, 1, amqp_empty_bytes, 0, 0, 0, 1, amqp_empty_table
     );
@@ -64,7 +70,7 @@ void queue_declare(SEXP xptr) {
 void consume(SEXP xptr) {
     amqp_rpc_reply_t res;
     amqp_envelope_t envelope;
-    amqp_connection_state_t conn = rabbitmq_get_state(xptr);
+    amqp_connection_state_t conn = get_connection_state(xptr);
     amqp_maybe_release_buffers(conn);
 
     res = amqp_consume_message(conn, &envelope, NULL, 0);
