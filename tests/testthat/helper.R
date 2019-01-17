7f053dde369412fe121cab4b59e76f713dfbca52
Lucas Cardozo <lucasecardozo@gmail.com>
2019-01-17 18:32:53 -0200

    trying to add OSX on build travis-ci build matrix

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,7 +1,9 @@
 skip_if_no_rabbitmq <- function() {
     testthat::skip_on_cran()
-    if (identical(Sys.getenv("TRAVIS"), "true")) {
-        return(NULL)
+    if (identical(Sys.getenv("TRAVIS"), "true")){
+        if (identical(Sys.getenv("TRAVIS_OS_NAME"), "linux")) {
+            return(NULL)
+        }
     }
     testthat::skip("RabbitMQ server is not available")
 }
